e8817cf0b35307f193f5503e190ba5f21d056b8a
aswin123456789 <aswinkalathur11@gmail.com.com>
2023-06-22 00:31:58 +0530

    stack

--- stack.js
+++ stack.js
@@ -7,26 +7,69 @@ class Node{
 
 class linkedListStack{
     constructor(){
-        this.first=null;
-        this.last=null;
+        this.firstOut=null;
+        this.lastOut=null;
         this.size=0;
+    }
+
+    empty(){
+        return this.size===0;
     }
 
     push(value){
         let node =new Node(value);
-        if(!this.first){
-            this.first=node;
-            this.last=node;
+        if(this.empty()){
+            this.firstOut=node;
+            this.lastOut=node;
         }
         else {
-            node.next=this.first;
-            this.first=node;
+            node.next=this.firstOut;
+            this.firstOut=node;
         }
         this.size++;
         return `${value}`
     }
+
+    peek(){
+        if(this.empty()){
+            return "empty stack"
+        }
+
+        return this.firstOut.value
+    }
+    
+    pop(){
+        if(this.empty()){
+            return "empty stack"
+        }
+        let curr=this.firstOut;
+        this.firstOut=curr.next;
+        curr.next=null;
+        return "popped"
+    }
+
+    display(){
+        
+        let curr=this.firstOut;
+        let values='';
+        while(curr){
+            values += curr.value +" ";
+            curr=curr.next;
+        }
+        console.log(values)
+        return;
+
+    }
 }
 
 let stack= new linkedListStack ();
-console.log(stack.push(12));
-console.log(stack.push(1));
+stack.push(12)
+stack.push(1)
+stack.push(11)
+
+stack.display()
+stack.pop()
+stack.display()
+console.log("last out is ",stack.peek());
+
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+console.log(stack.push(12));
+// console.log(stack.push(1));
+// console.log(stack.push(11));
